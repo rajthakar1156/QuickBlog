e0453cd8eae2dd6e2e1f0a7ab9b32fcc97539fdb
rajthakar1156 <rajthakar1156@gmail.com>
2025-06-28 13:37:59 +0530

    URL correction

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -87,7 +87,7 @@ const AddBlog = () => {
       <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded'>
         <p>Upload thumbnail</p>
         <label htmlFor="image">
-          <img src={!image ? assets.upload_area : URL.revokeObjectURL(image)} alt="" className='mt-2 h-16 rounded cursor-pointer'/>
+          <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt="" className='mt-2 h-16 rounded cursor-pointer'/>
           <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required />
 
         </label>
